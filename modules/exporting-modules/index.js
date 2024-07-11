35bdacaf055f5cf6356664861872bbdf4e887def
Tri <superglue2294@gmail.com>
2024-07-10 23:56:13 -0400

    completed the modules exercises

--- modules/exporting-modules/index.js
+++ modules/exporting-modules/index.js
@@ -1,1 +1,6 @@
+const practice = require('./practiceExports.js');
 // Import the modules exported from practiceExports.js below:
+
+console.log(typeof practice);
+console.log(practice.isPalindrome('mom'));
+console.log(practice.evenOrOdd(3));
