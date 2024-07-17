77c06dd7f921de5ed1f4471e79a8e3fa9135ad73
Tri <superglue2294@gmail.com>
2024-07-17 13:30:44 -0400

    exceptions exercises complete

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -5,3 +5,13 @@
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
 
 // Code your divide function here:
+
+let divide = (numerator, denominator) => {
+    if (denominator === 0) {
+        throw Error("Attempted to divide by zerio.");
+    }
+
+    return numerator / denominator;
+}
+
+console.log(divide(5,0));
