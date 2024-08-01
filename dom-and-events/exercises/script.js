1bb47e5bc01473d6bc8db9d17107c2f9c962de6b
Tri <superglue2294@gmail.com>
2024-08-01 02:06:27 -0400

    completed dom-and-events exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -5,6 +5,24 @@ function init () {
 
     // Put your code for the exercises here.
     
+    button.addEventListener("click", (e) => {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", (e) => {
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseleave", (e) => {
+        missionAbort.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener("click", (e) => {
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if (response) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    });
 }
 
 window.addEventListener("load", init);
