14fad90c8ce1770dc0279d5c2d10b195e93c60f2
Tri <superglue2294@gmail.com>
2024-08-08 19:14:25 -0400

    finished user-input-with-forms exercises

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,15 @@
 //Code Your Solution Below
+
+window.addEventListener("load", () => {
+    
+    let form = document.querySelector("form");
+    form.addEventListener("submit", e => {
+        const testName = document.getElementById("testName");
+        const testDate = document.getElementById("testDate");
+        const boosterCount = document.getElementById("boosterCount");
+        if (testName === "" || testDate === "" || boosterCount === "") {
+            e.preventDefault();
+        }
+    })
+
+})
